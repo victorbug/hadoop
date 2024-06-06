768c865b490617eafe0c331999911ca84fb330ec
victorbug <68400642+victorbug@users.noreply.github.com>
2024-06-06 19:18:10 -0400

    Add files via upload

---
+++ MapReduce/ItemSort/ItemSort_1Mapper.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class ItemSort_1Mapper extends Mapper<LongWritable, Text, IntWritable,
+IntWritable> {
+@Override
+     public void map(LongWritable key, Text value, Context context)
+throws IOException, InterruptedException {
+         String line = value.toString();
+           String[] tokens = line.split(","); // This is the delimiter between
+           int keypart = Integer.parseInt(tokens[0]);
+           int valuePart = Integer.parseInt(tokens[1]);
+           context.write(new IntWritable(valuePart), new IntWritable(keypart));
+
+
+
+        }
+}
