4d5982b6f4a96be36b5b54f3d9be4a5174d14846
victorbug <68400642+victorbug@users.noreply.github.com>
2024-06-06 19:18:42 -0400

    Add files via upload

---
+++ MapReduce/ItemSort/ItemSort_2Reducer_.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ItemSort_2Reducer_ extends Reducer<IntWritable, IntWritable,
+IntWritable, IntWritable> {
+@Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values,
+Context context)  throws IOException, InterruptedException {
+
+        for (IntWritable value : values) {
+
+            context.write(value,key);
+
+           }
+
+}
+}
